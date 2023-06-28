21cfdf1745fb87a294e6579c4366ecc9e1931710
Alexandre Delanoë <devel+git@delanoe.org>
2023-06-28 10:19:26 +0200

    [FIX] update schema

--- devops/postgres/schema.sql
+++ devops/postgres/schema.sql
@@ -357,3 +357,87 @@ ON context_node_ngrams(context_id, ngrams_id);
 
 CREATE INDEX IF NOT EXISTS node_stories_ngrams_id_idx
   ON node_stories(ngrams_id);
+----
+
+
+
+-- Version 0.0.6.9.9.6.4.sql
+-- ADD triggers
+ CREATE OR REPLACE FUNCTION check_node_stories_json()
+  RETURNS TRIGGER AS $$
+  DECLARE
+    missing_ngrams_exist boolean;
+  BEGIN
+      WITH child_ngrams as
+        (SELECT jsonb_array_elements_text(NEW.ngrams_repo_element->'children') AS term),
+        parent_ngrams AS
+        (SELECT NEW.ngrams_repo_element->>'root' AS term),
+
+        ngrams_child_parent AS
+            (SELECT child_ngrams.term, ngrams.terms
+         FROM child_ngrams
+         LEFT JOIN ngrams ON child_ngrams.term = ngrams.terms
+         WHERE ngrams.terms IS NULL
+
+        UNION
+
+        SELECT parent_ngrams.term, ngrams.terms
+        FROM parent_ngrams
+        LEFT JOIN ngrams ON parent_ngrams.term = ngrams.terms
+        WHERE ngrams.terms IS NULL
+        AND parent_ngrams.term IS NOT NULL)
+
+            SELECT EXISTS(SELECT * FROM ngrams_child_parent) INTO missing_ngrams_exist;
+
+            IF missing_ngrams_exist THEN
+              RAISE EXCEPTION 'node_stories: ngrams are missing: %', row_to_json(NEW);
+        END IF;
+
+            RETURN NEW;
+  END;
+  $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_node_stories_json_trg
+AFTER INSERT OR UPDATE
+ON node_stories
+FOR EACH ROW
+EXECUTE PROCEDURE check_node_stories_json();
+
+
+   CREATE OR REPLACE FUNCTION check_ngrams_json()
+    RETURNS TRIGGER AS $$
+    DECLARE
+      missing_ngrams_exist boolean;
+    BEGIN
+        WITH child_ngrams as
+          (SELECT jsonb_array_elements_text(ngrams_repo_element->'children') AS term
+           FROM node_stories
+           WHERE term = OLD.terms),
+          parent_ngrams AS
+          (SELECT ngrams_repo_element->>'root' AS term
+           FROM node_stories
+           WHERE term = OLD.terms),
+          child_parent_ngrams AS
+          (SELECT * FROM child_ngrams
+           UNION SELECT * FROM parent_ngrams)
+
+       SELECT EXISTS(SELECT * FROM child_parent_ngrams) INTO missing_ngrams_exist;
+
+       IF missing_ngrams_exist THEN
+          RAISE EXCEPTION 'ngrams are missing: %', row_to_json(OLD);
+       END IF;
+
+       RETURN OLD;
+    END;
+    $$ LANGUAGE plpgsql;
+
+  CREATE OR REPLACE TRIGGER check_ngrams_json_trg
+  AFTER DELETE
+  ON ngrams
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_ngrams_json();
+
+
+
+
+
