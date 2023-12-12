821d9677e29f1eefdd1ab0366b3d98127bdd1f86
Alfredo Di Napoli <alfredo@well-typed.com>
2023-12-12 11:10:59 +0100

    Add the timeMeasured function
    It's a useful function to log (with `DEBUG` severity) the execution time
    of various functions. Debug logs won't be printed in production.

--- src/Gargantext/Utils/UTCTime.hs
+++ src/Gargantext/Utils/UTCTime.hs
@@ -11,6 +11,7 @@ Portability : POSIX
 
 {-# LANGUAGE DeriveAnyClass             #-}
 {-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Gargantext.Utils.UTCTime where
@@ -19,10 +20,14 @@ import Data.Aeson (FromJSON, ToJSON)
 import Data.Morpheus.Kind (SCALAR)
 import Data.Morpheus.Types (GQLType(..), DecodeScalar(..), EncodeScalar(..))
 import Data.Morpheus.Types qualified as DMT
+import Data.String (fromString)
 import Data.Swagger (ToSchema)
 import Data.Text qualified as T
 import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX (getPOSIXTime)
 import Gargantext.Prelude
+import Gargantext.System.Logging
+import Prelude (String)
 
 
 newtype NUTCTime = NUTCTime UTCTime
@@ -39,3 +44,27 @@ instance GQLType NUTCTime where
 instance FromJSON NUTCTime
 instance ToJSON NUTCTime
 instance ToSchema NUTCTime
+
+timeMeasured :: (MonadLogger m, MonadBase IO m, HasCallStack)
+             => String
+             -- ^ A label
+             -> m a
+             -- ^ The action to run
+             -> m a
+timeMeasured = withFrozenCallStack $ timeMeasured' DEBUG
+
+timeMeasured' :: (MonadLogger m, MonadBase IO m, HasCallStack)
+             => LogLevel
+             -- ^ The severity of the log
+             -> String
+             -- ^ A label to identify the action.
+             -> m a
+             -- ^ The action to run
+             -> m a
+timeMeasured' severity label action = withFrozenCallStack $ do
+  startTime <- liftBase getPOSIXTime
+  res       <- action
+  endTime   <- liftBase getPOSIXTime
+  let msg = label <> " took " <> (show $ endTime - startTime) <> " seconds to execute."
+  $(logLocM) severity (fromString msg)
+  return res
