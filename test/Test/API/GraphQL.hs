c5a8606f2a2ae5461ff412bb6a16422edf5870ae
Przemysław Kaminski <pk@intrepidus.pl>
2024-02-05 18:58:05 +0100

    [test] tests for graphql errors, some fixes as well

--- test/Test/API/GraphQL.hs
+++ test/Test/API/GraphQL.hs
@@ -10,10 +10,11 @@ module Test.API.GraphQL (
 import Gargantext.Core.Types.Individu
 import Prelude
 import Servant.Auth.Client ()
-import Test.API.Private (withValidLogin, protected)
+import Test.API.Private (withValidLogin, protected, protectedNewError)
 import Test.API.Setup (withTestDBAndPort, setupEnvironment, createAliceAndBob)
 import Test.Hspec
 import Test.Hspec.Wai.Internal (withApplication)
+import Test.Hspec.Wai.JSON (json)
 import Test.Utils
 import Text.RawString.QQ (r)
 
@@ -30,6 +31,31 @@ tests = sequential $ aroundAll withTestDBAndPort $ do
 
         withApplication app $ do
           withValidLogin port "alice" (GargPassword "alice") $ \token -> do
-            protected token "POST" "/gql" [r| {
-               "query": "{ user_infos(user_id: 2) { ui_id, ui_email } }"
-               } |] `shouldRespondWithFragment` [jsonFragment| {"data":{"user_infos":[{"ui_id":2,"ui_email":"alice@gargan.text"}]}} |]
+            let query = [r| { "query": "{ user_infos(user_id: 2) { ui_id, ui_email } }" } |]
+            let expected = [json| {"data":{"user_infos":[{"ui_id":2,"ui_email":"alice@gargan.text"}]}} |]
+            protected token "POST" "/gql" query `shouldRespondWithFragment` expected
+
+
+    describe "check error format" $ do
+      it "returns the new error if header X-Garg-Error-Scheme: new is passed" $ \((_testEnv, port), app) -> do
+        withApplication app $ do
+          withValidLogin port "gargantua" (GargPassword "secret_key") $ \token -> do
+            let query = [r| { "query": "{ languages(id:5) { lt_lang } }" } |]
+            let expected = [json| {"errors": [{"locations":[{"column":13,"line":1}],"message":"Unknown Argument \"id\" on Field \"languages\"."}] } |]
+            protectedNewError token "POST" "/gql" query `shouldRespondWithFragment` expected
+
+      it "returns the old error (though this is deprecated)" $ \((_testEnv, port), app) -> do
+        withApplication app $ do
+          withValidLogin port "gargantua" (GargPassword "secret_key") $ \token -> do
+            let query = [r| { "query": "{ languages(id:5) { lt_lang } }" } |]
+            let expected = [json| {"errors": [{"locations":[{"column":13,"line":1}],"message":"Unknown Argument \"id\" on Field \"languages\"."}] } |]
+            protected token "POST" "/gql" query `shouldRespondWithFragment` expected
+
+      it "check new errors with 'type'" $ \((_testEnv, port), app) -> do
+        withApplication app $ do
+          withValidLogin port "gargantua" (GargPassword "secret_key") $ \token -> do
+            let query = [r| { "query": "mutation { delete_team_membership(shared_folder_id:1, team_node_id:1, token:\"abc\") }" } |]
+            let expected = [json| {"errors":[{"extensions":{"data":{"msg":"This user is not team owner","user_id":1},"diagnostic":"User not authorized. ","type":"EC_403__user_not_authorized"},"message":"User not authorized. "}]} |]
+            shouldRespondWithFragmentCustomStatus 403
+              (protectedNewError token "POST" "/gql" query)
+              expected
