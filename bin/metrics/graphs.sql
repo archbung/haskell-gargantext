c94bc775b503d771da6bab98c236002cdfb2e13f
Alexandre Delanoë <devel+git@delanoe.org>
2024-02-29 11:14:37 +0100

    [FIX] Metrics

--- bin/metrics/graphs.sql
+++ bin/metrics/graphs.sql
@@ -1,7 +1,7 @@
 WITH total AS (SELECT * from nodes n where n.typename = 9)
-    , sum AS (SELECT count(*) from total)
+    , sum AS (SELECT count(*) AS "TOTAL" from total)
     , increase AS (SELECT count(*) from total as t WHERE t.date >= date_trunc('month', current_date - interval '3' month))
 SELECT *,
-  (SELECT (100 * (SELECT * from increase) / (SELECT * from sum))) AS "LAST 3 MONTHS / TOTAL"
+  (SELECT TO_CHAR(((100 * (SELECT * from increase) / (SELECT * from sum))), 'fm00%')  AS "CREATED LAST 3 MONTHS")
      FROM sum
 
