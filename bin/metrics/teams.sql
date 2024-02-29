82a333db93233ea42c93bb2fff53534b419aaf85
Alexandre Delanoë <devel+git@delanoe.org>
2024-02-29 11:39:41 +0100

    [FIX] If no activity then null

--- bin/metrics/teams.sql
+++ bin/metrics/teams.sql
@@ -2,7 +2,7 @@ WITH total AS (SELECT * from nodes n where n.typename = 210)
     , sum AS (SELECT count(*) AS "TOTAL" from total)
     , increase AS (SELECT count(*) from total as t WHERE t.date >= date_trunc('month', current_date - interval '3' month))
 SELECT *,
-  (SELECT TO_CHAR((ROUND(100 * (SELECT * from increase) / (SELECT * from sum))), 'fm99%')  AS "CREATED LAST 3 MONTHS")
+  (SELECT TO_CHAR((ROUND(100 * NULLIF((SELECT * from increase),0) / NULLIF((SELECT * from sum), 0))), 'fm99%')  AS "CREATED LAST 3 MONTHS")
      FROM sum
 
 
