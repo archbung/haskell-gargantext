73429956092aa46d6168385a04afdbb81ab99d4e
Alexandre Delanoë <devel+git@delanoe.org>
2024-02-29 11:31:31 +0100

    [METRICS] better formating

--- bin/metrics/users.sql
+++ bin/metrics/users.sql
@@ -2,7 +2,7 @@ WITH total AS (SELECT * from auth_user as A)
     , sum AS (SELECT count(*) AS "TOTAL" from total)
     , increase AS (SELECT count(*) from total as t WHERE t.date_joined >= date_trunc('month', current_date - interval '3' month))
 SELECT *,
-  (SELECT TO_CHAR((ROUND(100 * (SELECT * from increase) / (SELECT * from sum))), 'fm0D00%')  AS "CREATED LAST 3 MONTHS")
+  (SELECT TO_CHAR((ROUND(100 * (SELECT * from increase) / (SELECT * from sum))), 'fm99%')  AS "CREATED LAST 3 MONTHS")
      FROM sum
 
 
